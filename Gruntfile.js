19263476a7856e311b13da3e22ffd0e1663fbad4
kylos101 <kylos101@gmail.com>
2015-01-11 19:21:39 -0500

    Added grunt-contrib-watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,8 +7,20 @@ module.exports = function(grunt) {
         dest: "generated/js/app.min.js",
         src: [
           "vendor/js/test.js",
-          "js/main.js"
+          "js/main.js",
+          "js/config/**/*.js",
+          "js/data/**/*.js",
+          "js/services/**/*.js",
+          "js/**/*.js"
         ]
+      }
+    },
+    watch: {
+      // notice how watch.app.files (below) self-references concat.app.src (above)
+      // what this does: when a change occurs, it runs the concat task.
+      app: {
+        files: ["<%= concat.app.src %>"],
+        tasks: ["concat"]
       }
     }
   }
@@ -21,9 +33,12 @@ module.exports = function(grunt) {
 
   // load node packages that are grunt plugins
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   // Note for the reader: concatentation is an alternative to require.js or common.js
   // concatentation is for small to medium stuff...be pragmatic.
 
   // a default task (so you don't have to specify one at the CLI)
-  grunt.registerTask('default', ['concat']);
+  grunt.registerTask('default', ['concat','watch']);
+
+  //TODO: Unit tests, JS LINT, other.z
 };
--- js/main.js
+++ js/main.js
@@ -1,2 +1,3 @@
 var foo = "main";
 return foo;
+console.log("This is a change for the watch task to pick up. ")
